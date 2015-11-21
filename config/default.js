5b0b1773beb2240a490dc98232f3f5ac7ee16429
fujisawa-shin <fujisawa-shin@kayac.com>
2015-11-22 00:02:10 +0900

    平常時は不要なファイルがのこってた

--- config/default.js
+++ config/default.js
@@ -1,8 +1,5 @@
 module.exports = {
     port: 6666,
-    mongodb: {
-        url: 'mongodb://localhost/hinagata-backend'
-    },
     uid_try_max_count: 5,
     cookie_secret: 'hinagata-backend',
     locals: {
