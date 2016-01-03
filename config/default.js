4e9780a61b92185d30968eb612b4685e1dda6994
fujisawa-shin <fujisawa-shin@kayac.com>
2016-01-04 01:52:35 +0900

    chromeは6000番台が気に食わないらしい

--- config/default.js
+++ config/default.js
@@ -1,10 +1,10 @@
 module.exports = {
-    port: 6666,
+    port: 7000,
     uid_try_max_count: 5,
     cookie_secret: 'hinagata-express',
     locals: {
         title: 'hinagata-express',
         description: 'node.js service.',
-        url: 'http://localhost:6666/'
+        url: 'http://localhost:7000/'
     }
 };
